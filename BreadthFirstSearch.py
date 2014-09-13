536803b91c34a519cb0f3ad9fa188c84f1481fd1
pratikbhatia199 <pratik.bhatia.199@gmail.com>
2014-09-12 22:01:45 -0400

    BFS in python

--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -1,3 +1,36 @@
 __author__ = 'pratik'
 
-print "Do breadth First search"
+from collections import deque
+class GraphBFS:
+
+    set_visited = set()
+    queue = deque()
+
+    dict_graph = {'r':['s', 'v'],
+                  's':['r', 'w'],
+                  't':['u', 'w', 'x'],
+                  'u':['t', 'y'],
+                  'v':['r'],
+                  'w':['s', 't', 'x'],
+                  'x':['w', 't', 'y'],
+                  'y':['x', 'u']
+                  }
+
+    def bfs(self, node):
+            print node
+            self.set_visited.add(node)
+            for adj_value in self.dict_graph[node]:
+                if adj_value not in self.set_visited:
+                    if adj_value not in self.queue:
+                        self.queue.append(adj_value)
+
+            while(self.queue):
+                node = self.queue.popleft()
+                self.bfs(node)
+
+
+def main():
+    g_bfs = GraphBFS()
+    g_bfs.bfs('s')
+
+main()
