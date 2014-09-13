144cd1a0fbd5eb2966998b40a879a7558b89fbf9
pratikbhatia199 <pratik.bhatia.199@gmail.com>
2014-09-13 16:55:50 -0400

    Find Inorder successor

---
+++ BinaryTreeWithParent.py
@@ -0,0 +1,143 @@
+__author__ = 'pratik'
+__author__ = 'pratik'
+class Node:
+    left = None
+    right = None
+    data = None
+    parent = None
+    def __init__(self, data):
+        self.data = data
+
+class BinaryTree:
+    root=None
+
+    def get_root(self):
+        return self.root
+
+    def insert_root(self,node):
+        self.root = node
+
+    def get_parent_pointer(self, node):
+        return node.parent
+
+    def get_parent(self, root, data, parent):
+        if root.data == data:
+            parent = root
+            return parent
+
+        if root.right:
+            parent = self.get_parent(root.right, data, parent)
+            if parent:
+                if parent.data == data:
+                    return parent
+
+        if root.left:
+            parent = self.get_parent(root.left, data, parent)
+            if parent:
+                if parent.data == data:
+                        return parent
+
+        return parent
+
+    def get_max_depth(self, root):
+        if root == None:
+            return 0
+        return max(self.get_max_depth(root.left), self.get_max_depth(root.right)) + 1
+
+    def is_balanced(self, root, balanced):
+        if balanced == False:
+            return balanced
+        print root
+        if root == None:
+            return balanced
+        print root.data
+
+        balance = self.get_max_depth(root.left) == self.get_max_depth(root.right)
+        recursion = self.is_balanced(root.left, balanced) and self.is_balanced(root.right, balanced)
+        return balanced and balance and recursion
+
+    def inorder_traversal(self,root):
+        if not root:
+            return
+        self.inorder_traversal(root.left)
+        print root.data
+        self.inorder_traversal(root.right)
+
+    def get_inorder_successor(self, node):
+        if node.right:
+            current_node = node.right
+            while(current_node.left!=None):
+                current_node=current_node.left
+            return current_node
+        else:
+            current_node = node
+            parent_node = current_node.parent
+            while parent_node:
+                if current_node==parent_node.left:
+                    return parent_node
+                else:
+                    current_node = parent_node
+                    parent_node=current_node.parent
+
+
+
+
+
+
+
+
+def main():
+    list_binary_tree = [(2,1,5),(5,3,6), (3,'',4),]
+    bin_tree = BinaryTree()
+    root = Node(list_binary_tree[0][0])
+    bin_tree.insert_root(root)
+    new_root = bin_tree.get_root()
+    bin_tree.inorder_traversal(root)
+    for nodes in list_binary_tree:
+        parent_data = nodes[0]
+        left_node=None
+        right_node=None
+        if nodes[1]:
+            left_node = Node(nodes[1])
+        if nodes[2]:
+            right_node = Node(nodes[2])
+        parent=None
+        parent = bin_tree.get_parent(root, parent_data, parent)
+        if parent:
+            print "Got", parent, parent_data
+            if left_node:
+                print "Inserting left node: ", nodes[1]
+                parent.left = left_node
+                left_node.parent = parent
+                print left_node.parent.data, parent_data
+
+            if right_node:
+                print "Inserting right node: ", nodes[2]
+                parent.right = right_node
+                right_node.parent = parent
+                print right_node.parent.data, parent_data
+
+            print parent.left
+            print parent.right
+
+        else:
+            print parent_data, parent, 'not found'
+
+    bin_tree.inorder_traversal(bin_tree.root)
+    print "Max depth: ",bin_tree.get_max_depth(bin_tree.root)
+    value =  bin_tree.is_balanced(bin_tree.root, True)
+    print "Is balanced ", value
+    for i in range(1,7,1):
+        node = bin_tree.get_parent(bin_tree.root,i, None)
+        print node, i
+        print "Finding inorder successor for ", node.data
+        successor = bin_tree.get_inorder_successor(node)
+        if successor:
+            print successor.data
+
+
+
+
+main()
+
+
