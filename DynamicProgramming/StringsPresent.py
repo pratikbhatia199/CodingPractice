b325ca0c5bc4181720060822bc790a32d4cc0d04
pratikbhatia199 <pratik.bhatia.199@gmail.com>
2014-09-28 20:33:06 -0400

    Word splittable or not

--- DynamicProgramming/StringsPresent.py
+++ DynamicProgramming/StringsPresent.py
@@ -1,11 +1,26 @@
 __author__ = 'pratik'
 
-list_strings = ["sea", "seasame", "street"]
-input_string = "seasamestreet"
+list_strings = ["ABC","AB","D"]
+input_string = "ABCD"
 
 def isSplittable(string):
-    if len(string ==0):
+    print "Input: ",string
+    if (string) ==0:
+        return True
 
+    if string in list_strings:
+        print "Matched", string
+        return True
 
-    for i in range
+    for i in range(1,len(string)):
+        print "Inside loop"
+        print string[i:]
+        print string[:i]
+        print "calling"
+        if isSplittable(string[i:]) & isSplittable(string[:i]):
+            print "returning true", string[i:], string[:i]
+            return True
+    return False
 
+x = isSplittable(input_string)
+print x
