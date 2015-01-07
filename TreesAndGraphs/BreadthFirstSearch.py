7f9dbddfcd6abba9eadbe5c8debe9fb07e500fad
pratikbhatia199 <pratik.bhatia.199@gmail.com>
2015-01-07 00:51:42 -0500

    Updated BFS

--- TreesAndGraphs/BreadthFirstSearch.py
+++ TreesAndGraphs/BreadthFirstSearch.py
@@ -3,7 +3,7 @@ __author__ = 'pratik'
 from collections import deque
 class GraphBFS:
 
-    set_visited = set()
+
     queue = deque()
 
     dict_graph = {'r':['s', 'v'],
@@ -15,6 +15,19 @@ class GraphBFS:
                   'x':['w', 't', 'y'],
                   'y':['x', 'u']
                   }
+    set_visited = set()
+    set_visiting = set()
+    def bfs(self, queue):
+        root = queue.popleft()
+
+        for adj_node in self.dict_graph[root]:
+            if adj_node not in self.set_visiting:
+                if adj_node not in self.set_visited:
+                    self.set_visiting.add(adj_node)
+                    queue.append(adj_node)
+        print root
+        self.set_visited.add(root)
+
     """
     dict_graph = {'u':['v','x'],
               'v':['y'],
@@ -26,22 +39,26 @@ class GraphBFS:
     """
 
 
-    def bfs(self, node):
-            print node
-            self.set_visited.add(node)
-            for adj_value in self.dict_graph[node]:
-                if adj_value not in self.set_visited:
-                    if adj_value not in self.queue:
-                        self.queue.append(adj_value)
-
-            while(self.queue):
-                node = self.queue.popleft()
-                self.bfs(node)
+    # def bfs(self, node):
+    #         print node
+    #         self.set_visited.add(node)
+    #         for adj_value in self.dict_graph[node]:
+    #             if adj_value not in self.set_visited:
+    #                 if adj_value not in self.queue:
+    #                     self.queue.append(adj_value)
+    #
+    #         while(self.queue):
+    #             node = self.queue.popleft()
+    #             self.bfs(node)
 
 
 def main():
     g_bfs = GraphBFS()
-    g_bfs.bfs('u')
+
+    queue = deque()
+    queue.append('u')
+    while(queue):
+        g_bfs.bfs(queue)
 
 
 main()
